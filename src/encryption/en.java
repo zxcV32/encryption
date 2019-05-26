939bfaa1c87f134aeb5d8fc9d01bcca9066bd23a
zxcv32i <i14a23h19a@gmail.com>
2019-05-26 12:16:53 +0530

    First Push

---
+++ src/encryption/en.java
@@ -0,0 +1,47 @@
+package encryption;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class en {
+	public static void main(String[] args) throws Exception {
+	int i,c=33;
+	
+	System.out.println(enc());
+	System.exit(0);
+	
+	String[][] a=new String[2][94];
+		//a[][]={-1};
+		for(i=0;i<94;i++)
+			a[0][i]=Character.toString((char)c++);
+		for(i=0;i<94;i++)
+			a[1][i]=Integer.toString((int)(Math.random()*100));
+			for(i=0;i<93;i++)
+				System.out.println(a[0][i]+" "+a[1][i]);
+	 
+	}
+	
+	static String enc() {
+		String text = "Hello World";
+        String key = "TheStrongestPasswordInTheWorld!!"; // 256 bit key
+        byte[] encrypted = null;
+        try {
+            // Create key and cipher
+            Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            // encrypt the text
+            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            encrypted = cipher.doFinal(text.getBytes());
+            // decrypt the text
+            cipher.init(Cipher.DECRYPT_MODE, aesKey);
+            String decrypted = new String(cipher.doFinal(encrypted));
+            System.err.println(decrypted);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+		return (new String(encrypted));
+	}
+}
